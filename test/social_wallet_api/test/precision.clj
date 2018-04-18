86b1352e7fe6e9098c97c73bb8f15b2f9a9bcd7f
aspasia <aspasia.beneti@gmail.com>
2018-04-18 17:36:51 +0200

    WIP: fixed the NaN tests

--- test/social_wallet_api/test/precision.clj
+++ test/social_wallet_api/test/precision.clj
@@ -15,33 +15,39 @@
 
 (def Satoshi 0.00000001)
 
+(defn new-transaction-request [amount]
+  (h/app
+   (->
+    (mock/request :post "/wallet/v1/transactions/new")
+    (mock/content-type "application/json")
+    (mock/body  (cheshire/generate-string {:blockchain :mongo
+                                           :from-id "test-1"
+                                           :to-id "test-2"
+                                           :amount amount
+                                           :tags ["blabla"]})))))
+
 (against-background [(before :contents (h/init
                                         (config-read social-wallet-api.test.handler/test-app-name)
                                         social-wallet-api.test.handler/test-app-name))
                      (after :contents (h/destroy))]
+                    (facts "Check specific amounts"
+                           )
+                    
                     (facts "Check different doubles"
                            (for-all
                             [rand-double (gen/double* {:min Satoshi
                                                        :max 9999999999999999.99999999
-                                                       :Nan? false
+                                                       :NaN? false
                                                        :infinite? false})]
-                            {:num-tests 100
-                             #_:max-size #_20
+                            {:num-tests 1000
+                             :max-size 200
                              #_:seed #_1510160943861}
                             (fact "A really large number with 16,8 digits"
                                   (let [amount (str rand-double)  
-                                       response (h/app
-                                                 (->
-                                                  (mock/request :post "/wallet/v1/transactions/new")
-                                                  (mock/content-type "application/json")
-                                                  (mock/body  (cheshire/generate-string {:blockchain :mongo
-                                                                                         :from-id "test-1"
-                                                                                         :to-id "test-2"
-                                                                                         :amount (log/spy amount)
-                                                                                         :tags ["blabla"]}))))
+                                       response (new-transaction-request amount)
                                        body (parse-body (:body response))]
                                    (:status response) => 200
-                                   (:amount body) => rand-double)))
+                                  (:amount body) => rand-double)))
                            
 
                            #_(fact "Check other inputs"
